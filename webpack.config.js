fb0ada70a77458cc374689db9da3c022f950c39a
Jonas Mai <git-commits@jonasmai.de>
2021-01-18 20:54:26 +0100

    updates versions, adds devserver

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ module.exports = {
   mode: 'development',
   devtool: 'source-map',
   entry: {
-    drinks: './app/static/index.ts',
-    orders: './app/static/orders/index.ts',
+    drinks: './app/index.ts',
+    orders: './app/orders/index.ts',
   },
   output: {
     path: path.resolve(__dirname, 'app/dist'),
@@ -17,12 +17,12 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
-        { from: './app/static/site.webmanifest', to: '' },
-        { from: './app/static/fonts', to: 'fonts' },
-        { from: './app/static/style', to: 'style' },
-        { from: './app/static/img', to: 'img' },
-        { from: './app/static/index.html', to: '' },
-        { from: './app/static/orders/index.html', to: 'orders' },
+        { from: './app/site.webmanifest', to: '' },
+        { from: './app/fonts', to: 'fonts' },
+        { from: './app/style', to: 'style' },
+        { from: './app/img', to: 'img' },
+        { from: './app/index.html', to: '' },
+        { from: './app/orders/index.html', to: 'orders' },
       ],
     }),
   ],
@@ -38,5 +38,10 @@ module.exports = {
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { test: /\.js$/, loader: 'source-map-loader' },
     ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'app/dist'),
+    compress: true,
+    port: 9000,
   },
 };
